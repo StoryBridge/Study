972b6395a79289c4f03d20e9aac754f2a9aad20a
이성현 <zzennam@naver.com>
2019-11-15 17:18:46 +0900

    Update Person.java
    접근제어자

--- java/BankAccount/Person.java
+++ java/BankAccount/Person.java
@@ -1,6 +1,33 @@
 public class Person {
-    String name;
-    int age;
-    int cashAmount;
-    BankAccount account;
+    private String name;
+    private int age;
+    private int cashAmount;
+    private BankAccount account;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getCashAmount() {
+		return cashAmount;
+	}
+	public void setCashAmount(int cashAmount) {
+		this.cashAmount = cashAmount;
+	}
+	public BankAccount getAccount() {
+		return account;
+	}
+	public void setAccount(BankAccount account) {
+		this.account = account;
+	}
+    
+
 }
