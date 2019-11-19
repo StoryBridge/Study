ab11d8df859bf7f4cc455f883926a90263a39373
이성현 <zzennam@naver.com>
2019-11-19 20:03:52 +0900

    Update BankAccount.java

--- java/BankAccount/AccessModifier/BankAccount.java
+++ java/BankAccount/AccessModifier/BankAccount.java
@@ -1,89 +1,85 @@
 package accessmodifier;
 
-public class BankAccount {
-	private int balance;
-	private Person owner;
+import java.awt.image.BandCombineOp;
 
-	// 파라미터 : 입금할 액수(정수)
-	// 리턴 : 성공여부(불린)
-	boolean deposit(int amount) {
-		// 1. write code here
-		if (amount < 0 || owner.getCashAmount() < amount) {
-			System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
-			return false;
-		} else {
-			balance += amount;
-			owner.setCashAmount(owner.getCashAmount() - amount);
-			System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
-			return true;
-		}
+public class Person {
+	private String name;
+	private int age;
+	private int cashAmount;
+	private BankAccount account;
+
+	public String getName() {
+		return name;
 	}
 
-	// 파라미터 : 출금할 액수(정수)
-	// 리턴 : 성공여부(불린)
-	boolean withdraw(int amount) {
-		// 2. write code here
-		if (amount < balance && amount >= 0) {
-			balance -= amount;
-			owner.setCashAmount(owner.getCashAmount() + amount);
-			System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
-			return true;
-		} else {
-			System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
-			return false;
-		}
-
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public int getBalance() {
-		return balance;
+	public int getAge() {
+		return age;
 	}
 
-	public void setBalance(int balance) {
-		this.balance = balance;
+	public void setAge(int age) {
+		this.age = age;
 	}
 
-	public Person getOwner() {
-		return owner;
+	public int getCashAmount() {
+		return cashAmount;
 	}
 
-	public void setOwner(Person owner) {
-		this.owner = owner;
+	public void setCashAmount(int cashAmount) {
+		this.cashAmount = cashAmount;
+	}
+
+	public BankAccount getAccount() {
+		return account;
+	}
+
+	public void setAccount(BankAccount account) {
+		this.account = account;
 	}
 
 	// 첫 번째 파라미터: 받는 사람 (Person)
 	// 두 번째 파라미터: 이체할 금액 (정수)
 	// 리턴 : 성공여부 (불린)
 	public boolean transfer(Person to, int amount) {
-		boolean success;
-		if (amount < 0 || amount > balance) {
-			success = false;
-		} else {
-			balance = balance - amount;
-			to.getAccount().setBalance(to.getAccount().getBalance() + amount);
-			success = true;
-		}
-		// 결과값 출력
-		System.out.println(success + " - from: " + owner.getName() + ", to: " + to.getName() + ", amount: " + amount
-				+ ", balance: " + balance);
-		return success;
+		return account.transfer(to.getAccount(), amount);
 	}
+	
+	public boolean transfer(BankAccount to, int amount) {
+		return account.transfer(to, amount);
+	}
+//	public boolean transfer(Person to, int amount) {
+//		boolean success;
+//		if (amount < 0 || amount > account.getBalance()) {
+//			success = false;
+//		} else {
+//			account.setBalance(account.getBalance() - amount);
+//			to.getAccount().setBalance(to.getAccount().getBalance() + amount);
+//			success = true;
+//		}
+//		// 결과값 출력
+//		System.out.println(success + " - from: " + name + ", to: " + to.getName() + ", amount: " + amount
+//				+ ", balance: " + account.getBalance());
+//		return success;
+//	}
 
 	// 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
 	// 두 번째 파라미터: 이체할 금액 (정수)
 	// 리턴 : 성공여부 (불린)
-	public boolean transfer(BankAccount to, int amount) {
-		boolean success;
-		if (amount < 0 || amount > balance) {
-			success = false;
-		} else {
-			balance -= amount;
-			to.setBalance(to.getBalance() + amount);
-			success = true;
-		}
-		// 결과값 출력
-		System.out.println(success + " - from: " + owner.getName() + ", to: " + to.getOwner().getName() + ", amount: " + amount
-				+ ", balance: " + balance);
-		return success;
-	}
+//	public boolean transfer(BankAccount to, int amount) {
+//		boolean success;
+//		if (amount < 0 || amount > account.getBalance()) {
+//			success = false;
+//		} else {
+//			account.setBalance(account.getBalance() - amount);
+//			to.setBalance(to.getBalance() + amount);
+//			success = true;
+//		}
+//		// 결과값 출력
+//		System.out.println(success + " - from: " + name + ", to: " + to.getOwner().getName() + ", amount: " + amount
+//				+ ", balance: " + account.getBalance());
+//		return success;
+//	}
 }
